28ed00355d59684c8cea9cbad5161f7e2ed0e101
Theo Wlasiczuk Ballesteros <theoballesterosok@gmail.com>
2026-06-19 21:34:13 +0000

    agregar archivos iniciales y base del codigo

---
+++ main.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+import csv
+
+archivo = "database.csv"
+campos = ["", ""]
+
+def leer_archivos():
+    pass
+def guardar_archivos():
+    pass
+def chatbot():
+    pass
+def auth():
+    pass
